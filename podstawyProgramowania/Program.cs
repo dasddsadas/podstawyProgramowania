df309ba77bd9904da62d26f29129e745c6333da4
Bartosz Sznajder <bs317784@student.polsl.pl>
2025-05-28 20:45:47 +0200

    task-2

--- podstawyProgramowania/Program.cs
+++ podstawyProgramowania/Program.cs
@@ -1,6 +1,20 @@
-﻿int num1 = 4;
+﻿//task 1
+
+int num1 = 4;
 int num2 = 2;
 int div =num1 / num2;
 
 Console.WriteLine(div);
+
+//task 2
+
+int x = 3;
+int y = 5;
+int temp = y;
+y = x;
+x = temp;
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+//task 3
 
