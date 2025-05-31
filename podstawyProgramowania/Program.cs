97cabdf0cc20de4edeb35fb9215f7c99dd73edd9
Bartosz Sznajder <bs317784@student.polsl.pl>
2025-05-31 20:39:13 +0200

    usuniete komentarze, zmianna nazwy zmiennych na bardziej czytelne

--- podstawyProgramowania/Program.cs
+++ podstawyProgramowania/Program.cs
@@ -1,12 +1,8 @@
-﻿//task 10
+﻿int number = int.Parse(Console.ReadLine());
 
-//sprawdza czy liczba jest w odleglosci 20 od 100 albo 200
-
-int num1 = int.Parse(Console.ReadLine());
-
-bool inRange100 = num1 >= 80 && num1 <= 120;
-bool inRange200 = num1 >= 180 && num1 <= 220;
-if ((inRange100) || (inRange200))
+bool InRange20Of100 = number >= 80 && number <= 120;
+bool inRange20Of200 = number >= 180 && number <= 220;
+if ((InRange20Of100) || (inRange20Of200))
 {
     Console.WriteLine("liczba jest wieksza lub mniejsza od 100 lub 200 o 20");
 }
